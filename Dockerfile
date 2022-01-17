b7eb2d9d23dedda3a9ff7054bd06de4bacfa8745
a.v.zalesov <a.v.zalesov@tmn3.etagi.com>
2022-01-17 09:07:18 +0500

    docker

--- Dockerfile
+++ Dockerfile
@@ -2,13 +2,20 @@ FROM node
 
 WORKDIR /app
 
-COPY package.json /app
+COPY package*.json /app/
+
+COPY tsconfig.json /app
+
+RUN npm install -g typescript
 
 RUN npm install
 
-Copy . .
+CMD npm run build
+
+COPY . /app
+
+RUN ls -a
 
 EXPOSE 5000
 
-CMD ["tsc"]
 CMD ["node", "dist/index.js"]
--- config/config.ts
+++ config/config.ts
@@ -11,17 +11,7 @@ const development = {
         dialect: "postgres"
     }
 }
-const production = {
-    username: process.env.DB_USERNAME,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    database_url: process.env.DATABASE_URL,
-    options: {
-        host: process.env.DB_HOST,
-        dialect: "postgres"
-    }
-}
+
 export {
-    development,
-    production
+    development
 }
